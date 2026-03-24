7715e69cfad708ee52480ca609c9882418d6dc38
Андрей <lbasalaeff@gmail.com>
2026-03-24 23:49:34 +0700

    feat(IncidentForm): add plug for IncidentForm

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Компоненты
 import Home from './pages/Home';
 import IncidentDetail from './pages/IncidentDetail';
-// import IncidentForm from './pages/IncidentForm';
+import IncidentForm from './pages/IncidentForm';
 // import Login from './pages/Login';
 // import Registration from './pages/Registration';
 
@@ -20,7 +20,7 @@ const App = () => {
         {/* Если http://localhost:5173/incident/:id тогда отобразится IncidentDetail(); */}
         <Route path='/incident/:id' element={<IncidentDetail />} />
         {/* Если http://localhost:5173/create-incident тогда отобразится IncidentForm(); */}
-        {/* <Route path='/create-incident' element={<IncidentForm />} /> */}
+        <Route path='/create-incident' element={<IncidentForm />} />
         {/* Если http://localhost:5173/login тогда отобразится Login(); */}
         {/* <Route path='/login' element={<Login />} /> */}
         {/* Если http://localhost:5173/create-account тогда отобразится Registration(); */}
