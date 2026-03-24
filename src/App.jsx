ba6fb63a5fcad0ac3e6938535c399751adaa2124
Андрей <lbasalaeff@gmail.com>
2026-03-24 21:52:04 +0700

    feat(map): add base map with user geolocation and OpenStreetMap tiles

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Компоненты
 import Home from './pages/Home';
-import IncidentDetail from './pages/IncidentDetail';
-import IncidentForm from './pages/IncidentForm';
-import Login from './pages/Login';
-import Registration from './pages/Registration';
+// import IncidentDetail from './pages/IncidentDetail';
+// import IncidentForm from './pages/IncidentForm';
+// import Login from './pages/Login';
+// import Registration from './pages/Registration';
 
 // Основная функция приложения
 
@@ -18,14 +18,16 @@ const App = () => {
         {/* Если http://localhost:5173/ тогда отобразится Home(); */}
         <Route path='/' element={<Home />} />
         {/* Если http://localhost:5173/incident/:id тогда отобразится IncidentDetail(); */}
-        <Route path='/incident/:id' element={<IncidentDetail />} />
+        {/* <Route path='/incident/:id' element={<IncidentDetail />} /> */}
         {/* Если http://localhost:5173/create-incident тогда отобразится IncidentForm(); */}
-        <Route path='/create-incident' element={<IncidentForm />} />
+        {/* <Route path='/create-incident' element={<IncidentForm />} /> */}
         {/* Если http://localhost:5173/login тогда отобразится Login(); */}
-        <Route path='/login' element={<Login />} />
+        {/* <Route path='/login' element={<Login />} /> */}
         {/* Если http://localhost:5173/create-account тогда отобразится Registration(); */}
-        <Route path='/create-account' element={<Registration />} />
+        {/* <Route path='/create-account' element={<Registration />} /> */}
       </Routes>
     </Router>
   );
 };
+
+export default App;
