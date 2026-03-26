ad8daf24a48f7417f02bfe7b6edf59473f1ea8cf
Андрей <lbasalaeff@gmail.com>
2026-03-26 23:33:15 +0700

    feat(toast): install library for animated notifications

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,23 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* Нужно обязательно создать контейнер для toast */}
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={true}
+      closeOnClick={true}
+      rtl={false} // Не трогай, если не араб!
+      pauseOnFocusLoss={false} //переключение на другую вкладку
+      draggable={true} //Смахнуть разрешаю
+      theme="colored" //еще есть light, dark, auto, colored (используется)
+    />
   </StrictMode>
 );
