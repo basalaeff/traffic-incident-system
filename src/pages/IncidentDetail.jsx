b615089a512401ce79af5f200fab0f888f47d393
Андрей <lbasalaeff@gmail.com>
2026-03-31 19:11:27 +0700

    feat(incident-detail-ui): develop a base ui and style, add queries for obtaining data

--- src/pages/IncidentDetail.jsx
+++ src/pages/IncidentDetail.jsx
@@ -1,40 +1,93 @@
+// стили
+import './IncidentDetail.css';
 //  Нужен хук, чтобы доставать id из ссылки
-import { use } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+// добавлю любимые всплывающие уведомления
+import { toast } from 'react-toastify';
+// запросы
+import axios from 'axios';
 
 function IncidentDetail() {
   // Достаем id c помощью хука
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  // Создадим массив для хранения инцидента
+  const [incident, setIncident] = useState([]);
+  const [user, setUser] = useState([]);
+
+  // загрузка
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchIncidents = async () => {
+      setIsLoading(true);
+      try {
+        // ============================================================================
+        // GET-ЗАПРОС НА СЕРВЕР http://localhost:3001/incidents
+        // ============================================================================
+        const responseIncidents = await axios.get(`http://localhost:3001/incidents/${id}`);
+        setIncident(responseIncidents.data);
+        // проверка перед запросом
+        if (responseIncidents.data?.userId) {
+          // ============================================================================
+          // GET-ЗАПРОС НА СЕРВЕР http://localhost:3001/user
+          // ============================================================================
+          const responseUser = await axios.get(
+            `http://localhost:3001/users/${responseIncidents.data.userId}`
+          );
+          setUser(responseUser.data);
+        } else {
+          toast.error('Ошибка загрузки данных во втором запросе');
+        }
+        setIsLoading(false);
+      } catch (err) {
+        console.error(`Ошибка загрузки данных:`, err);
+        toast.error('Ошибка загрузки данных');
+        toast.error(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchIncidents();
+  }, [id]);
+  // перезапускается при изменении id
+
   // Пишем интерфейс на JSX
   return (
-    // Такой же внешний контейнер как в home
-    <div style={{ padding: '20px' }}>
-      <h1>Детали инцидента №{id}</h1>
-      <div
-        style={{
-          border: '1px dashed #ccc',
-          padding: '20px',
-          textAlign: 'center',
-          color: '#666',
-          marginTop: '20px',
-        }}
-      >
-        Страница детализации находится в разработке
+    <div className="detail-page">
+      <div className="detail-card">
+        <div className="detail-header">
+          <h2>Детали инцидента </h2>
+        </div>
+        {/* detail-header */}
+        <div className="detail">
+          <div className="detail-title">{incident?.title}</div>
+          {/* detail-title */}
+          <div className="detail-description">{incident?.description}</div>
+          {/* detail-description */}
+          <div className="detail-type">{incident?.type}</div>
+          {/* detail-type */}
+          <div className="detail-position">
+            {incident?.lat}, {incident?.lng}
+          </div>
+          {/* detail-position */}
+          <div className="detail-user">{user?.email}</div>
+          {/* detail-user */}
+          <div className="detail-time">
+            {incident?.date}, {incident?.time}
+          </div>
+          {/* detail-time */}
+          <div className="detail-status">{incident?.status}</div>
+          {/* detail-status */}
+        </div>
+        {/* detail */}
       </div>
-      <button
-        onClick={() => window.history.back()}
-        style={{
-          marginTop: '20px',
-          padding: '8px 16px',
-          cursor: 'pointer',
-          backgroundColor: '#f0f0f0',
-          border: '1px solid #ccc',
-          borderRadius: '4px',
-        }}
-      >
-        Назад
-      </button>
+      {/* detail-card */}
     </div>
+    // detail-page
   );
 }
 export default IncidentDetail;
