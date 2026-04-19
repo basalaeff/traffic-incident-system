1a58257576a913545cd5818a468e2b514c47eff4
Андрей <lbasalaeff@gmail.com>
2026-04-19 19:46:19 +0700

    styles(burger-btns-menu): add field for button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -332,71 +332,78 @@ function Home() {
     // Внешний контейнер для всей страницы
     <div className="page-home">
       <div className="floating-btns">
-        {/* Кнопка ОТМЕНА */}
-        {displayFloatingBtn && isAddingMode && !displayLogout && (
-          <button
-            className="circle-btn"
-            onClick={handleCancelCreateIncident}
-            title="Отмена"
-            style={{ backgroundColor: 'var(--danger-color)' }}
-          >
-            <img src="https://s.kontur.ru/common-v2/icons-ui/black/x-circle/x-circle-32-Regular.svg" />
-          </button>
-        )}
-        {/* Кнопка ДОБАВЛЕНИЕ */}
-        {displayFloatingBtn && user && displayMap && !isAddingMode && !displayLogout && (
-          <button className="circle-btn" onClick={handleCreateIncident} title="Добавить инцидент">
-            <img src="https://s.kontur.ru/common-v2/icons-ui/black/plus-circle/plus-circle-32-Regular.png" />
-          </button>
-        )}
-        {/* Кнопка ЦЕНТИРОВАТЬ ПО ГЕОЛОКАЦИИ */}
-        {displayFloatingBtn && !displayLogout && displayMap && (
-          <button className="circle-btn" onClick={getLocation} title="Вернуться в координаты">
-            <img src="https://s.kontur.ru/common-v2/icons-ui/black/location-pin/location-pin-32-Regular.svg" />
-          </button>
-        )}
-        {/* Кнопка КАРТЫ */}
-        {displayFloatingBtn && !displayLogout && (
-          <button
-            className="circle-btn"
-            onClick={() => {
-              displayMap ? setDisplayMap(false) : setDisplayMap(true);
-            }}
-            title={displayMap ? 'Скрыть карту' : 'Показать карту'}
-          >
-            {displayMap ? (
-              <img src="https://s.kontur.ru/common-v2/icons-ui/black/building-home/building-home-32-Regular.svg" />
-            ) : (
-              <img src="https://s.kontur.ru/common-v2/icons-ui/black/location-map/location-map-32-Regular.svg" />
-            )}
-          </button>
-        )}
-        {/* Кнопка ВХОДА */}
-        {displayFloatingBtn && !displayLogout && (
-          <button className="circle-btn" onClick={handleAuthClick} title={user ? 'Выйти' : 'Войти'}>
-            {user ? (
-              <Avatar name={user?.login} size="46" round={true} />
-            ) : (
-              <img src="https://s.kontur.ru/common-v2/icons-ui/black/arrow-ui-auth-login/arrow-ui-auth-login-32-Regular.svg" />
-            )}
-          </button>
-        )}
-        {/* Кнопка МЕНЮ */}
-        {!displayLogout && (
-          <button
-            className="circle-btn"
-            onClick={() => {
-              displayFloatingBtn ? setDisplayFloatingBtn(false) : setDisplayFloatingBtn(true);
-            }}
-            title={user ? 'Скрыть' : 'Показать'}
-          >
-            {displayFloatingBtn ? (
-              <img src="https://s.kontur.ru/common-v2/icons-ui/black/arrow-shape-a-right/arrow-shape-a-right-32-Regular.svg" />
-            ) : (
-              <img src="https://s.kontur.ru/common-v2/icons-ui/black/ui-menu-bars-3-h/ui-menu-bars-3-h-32-Regular.svg" />
-            )}
-          </button>
-        )}
+        <div className="btns-menu">
+          {/* Кнопка ОТМЕНА */}
+          {displayFloatingBtn && isAddingMode && !displayLogout && (
+            <button
+              className="circle-btn"
+              onClick={handleCancelCreateIncident}
+              title="Отмена"
+              style={{ backgroundColor: 'var(--danger-color)' }}
+            >
+              <img src="https://s.kontur.ru/common-v2/icons-ui/black/x-circle/x-circle-32-Regular.svg" />
+            </button>
+          )}
+          {/* Кнопка ДОБАВЛЕНИЕ */}
+          {displayFloatingBtn && user && displayMap && !isAddingMode && !displayLogout && (
+            <button className="circle-btn" onClick={handleCreateIncident} title="Добавить инцидент">
+              <img src="https://s.kontur.ru/common-v2/icons-ui/black/plus-circle/plus-circle-32-Regular.png" />
+            </button>
+          )}
+          {/* Кнопка ЦЕНТИРОВАТЬ ПО ГЕОЛОКАЦИИ */}
+          {displayFloatingBtn && !displayLogout && displayMap && (
+            <button className="circle-btn" onClick={getLocation} title="Вернуться в координаты">
+              <img src="https://s.kontur.ru/common-v2/icons-ui/black/location-pin/location-pin-32-Regular.svg" />
+            </button>
+          )}
+          {/* Кнопка КАРТЫ */}
+          {displayFloatingBtn && !displayLogout && (
+            <button
+              className="circle-btn"
+              onClick={() => {
+                displayMap ? setDisplayMap(false) : setDisplayMap(true);
+              }}
+              title={displayMap ? 'Скрыть карту' : 'Показать карту'}
+            >
+              {displayMap ? (
+                <img src="https://s.kontur.ru/common-v2/icons-ui/black/building-home/building-home-32-Regular.svg" />
+              ) : (
+                <img src="https://s.kontur.ru/common-v2/icons-ui/black/location-map/location-map-32-Regular.svg" />
+              )}
+            </button>
+          )}
+          {/* Кнопка ВХОДА */}
+          {displayFloatingBtn && !displayLogout && (
+            <button
+              className="circle-btn"
+              onClick={handleAuthClick}
+              title={user ? 'Выйти' : 'Войти'}
+            >
+              {user ? (
+                <Avatar name={user?.login} size="46" round={true} />
+              ) : (
+                <img src="https://s.kontur.ru/common-v2/icons-ui/black/arrow-ui-auth-login/arrow-ui-auth-login-32-Regular.svg" />
+              )}
+            </button>
+          )}
+          {/* Кнопка МЕНЮ */}
+          {!displayLogout && (
+            <button
+              className="circle-btn"
+              onClick={() => {
+                displayFloatingBtn ? setDisplayFloatingBtn(false) : setDisplayFloatingBtn(true);
+              }}
+              title={user ? 'Скрыть' : 'Показать'}
+            >
+              {displayFloatingBtn ? (
+                <img src="https://s.kontur.ru/common-v2/icons-ui/black/arrow-shape-a-right/arrow-shape-a-right-32-Regular.svg" />
+              ) : (
+                <img src="https://s.kontur.ru/common-v2/icons-ui/black/ui-menu-bars-3-h/ui-menu-bars-3-h-32-Regular.svg" />
+              )}
+            </button>
+          )}
+        </div>
+        {/* btns-menu */}
       </div>
       {/* floating-btns */}
       {!displayMap && (
