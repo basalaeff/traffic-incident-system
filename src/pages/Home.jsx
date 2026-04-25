a96c901010c66f238b7e161c2082c86f1e83fb83
Андрей <lbasalaeff@gmail.com>
2026-04-25 16:01:58 +0700

    refactor(getCustomMarker): moved getCustomMarker in home/model (fsd)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,8 +14,6 @@ import { useNavigate } from 'react-router-dom';
 import '../css/components/map.css';
 import 'leaflet/dist/leaflet.css';
 
-// Нужна библиотека для работы с иконками
-import L from 'leaflet';
 import Avatar from 'react-avatar';
 
 // всплывающие уведомления тоже могут пригодиться
@@ -24,55 +22,8 @@ import { toast } from 'react-toastify';
 import { getCurrentUser, logoutUser } from '../features/authentication/model/auth';
 import { loadIncidents, loadIncidentCards, loadUsers } from '@/features/home/api/homeAPI';
 import { RecenterAutomatically } from '@/features/home/model/RecenterAutomatically';
+import { getCustomMarker } from '@/features/home/model/getCustomMarker';
 import { AnimationFadeInUp } from '../shared/ui/animation';
-
-// ============================================================================
-// НАСТРОЙКА МАРКЕРОВ
-// ============================================================================
-// Есть несколько типов инцидентов (пока ДТП и опасные участки, но может придумаю еще)
-// У каждого типа инцидента будет свой маркер
-// Напишу функцию, которая будет возвращать маркеры по типу инцидента
-// Взял с GitHub: https://github.com/pointhi/leaflet-color-markers
-
-const getCustomIcon = (type, status) => {
-  // Пропишу иконки
-  // Красный маркер (для ДТП)
-  const redIconUrl = '/markers/marker-icon-2x-red.png';
-  // Оранжевый маркер (для опасных участков)
-  const orangeIconUrl = '/markers/marker-icon-2x-orange.png';
-  // Зеленый маркер (для другое)
-  const greenIconUrl = '/markers/marker-icon-2x-green.png';
-  // возьмем серый для "Устранено"
-  const grayIconUrl = '/markers/marker-icon-2x-grey.png';
-  // Синий по умолчанию
-  const blueIconUrl = '/markers/marker-icon-2x-blue.png';
-
-  let chosenUrl = blueIconUrl; // По умолчанию ставим синюю
-
-  // Реализую логику выбора цвета
-  if (status === 'inactive') {
-    // Если инцидент устранен - ставим серую
-    chosenUrl = grayIconUrl;
-  } else if (type === 'accident') {
-    // Если тип accident - ставим красную
-    chosenUrl = redIconUrl;
-  } else if (type === 'hazard') {
-    // Если тип hazard - ставим оранжевую
-    chosenUrl = orangeIconUrl;
-  } else if (type === 'other') {
-    chosenUrl = greenIconUrl;
-  }
-  // А теперь просто возвращаем созданный объект
-  // Размеры взял с того же гит хаб
-  // Создал общую конструкцию для вывода иконок
-  return L.icon({
-    iconUrl: chosenUrl, // Ссылка на выбранный маркер
-    iconSize: [25, 41], // Размер иконки [ширина, высота] в пикселях.
-    iconAnchor: [12, 41], // Точка привязки
-    // 20 - это центр по горизонтали, 40 - это высота.
-    popupAnchor: [1, -34], // Отсюда открывается попап.
-  });
-};
 
 //  Нужно придумать как ловить клики по карте
 // ============================================================================
@@ -706,7 +657,7 @@ function Home() {
                 <Marker
                   key={incident?.id}
                   position={[incident?.lat, incident?.lng]}
-                  icon={getCustomIcon(incident?.type, incident?.status)}
+                  icon={getCustomMarker(incident?.type, incident?.status)}
                   // хочу реализовать, чтобы открывался при наведении
                   eventHandlers={{
                     mouseover: (e) => {
@@ -750,7 +701,7 @@ function Home() {
             {tempMarker && (
               <Marker
                 position={tempMarker}
-                icon={getCustomIcon('new', 'active')}
+                icon={getCustomMarker('new', 'active')}
                 eventHandlers={{
                   mouseover: (e) => {
                     e.target.openPopup();
