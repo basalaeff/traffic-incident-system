fa52542eaa736c6a1ee1ec262221dcd8337d4ea1
Андрей <lbasalaeff@gmail.com>
2026-04-26 12:35:03 +0700

    update(files): deleted files

--- src/animation.js
+++
@@ -1,9 +0,0 @@
-import styled, { keyframes } from 'styled-components';
-import { fadeInUp } from 'react-animations';
-// ============================================================================
-// АНИМАЦИЯ СНИЗУ ВВЕРХ
-// ============================================================================
-const fadeInUpKey = keyframes`${fadeInUp}`;
-export const AnimationFadeInUp = styled.div`
-  animation: 0.5s ${fadeInUpKey} ease-out forwards;
-`;
